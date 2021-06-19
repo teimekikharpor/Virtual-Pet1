dd20747fd51bd775c919512feef51a19a1050e6e
teimekikharpor <77444056+teimekikharpor@users.noreply.github.com>
2021-06-19 18:15:57 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,28 @@
+var Dog;
+var happyDog;
+var database;
+var foodS;
+var foodStock;
+
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  
+
+}
+
+
+
